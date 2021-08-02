c210c6a273ae06c800d7cb93e87c166e3c8da60f
Xman1109 <shawnoliver1109@gmail.com>
2021-08-03 00:18:48 +0200

    graphite

---
+++ prototypes/items/graphite.lua
@@ -0,0 +1,12 @@
+data:extend(
+    {
+        {
+            type = "item",
+            name = "graphite",
+            icon = "__Atomic_Overhaul__/graphics/icons/graphite.png",
+            icon_size = 64,
+            stack_size = 50,
+            group = "intermediate-products",
+            subgroup = "intermediate-product"
+        }
+    })
