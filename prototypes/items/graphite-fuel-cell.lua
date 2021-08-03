a4fd299d1446cf60fee0b3527ef080e776b7d196
Xman1109 <shawnoliver1109@gmail.com>
2021-08-03 17:10:57 +0200

    ,

--- prototypes/items/graphite-fuel-cell.lua
+++ prototypes/items/graphite-fuel-cell.lua
@@ -3,7 +3,7 @@ data:extend({
 		type = "item",
 		name = "graphite-fuel-cell",
 		icon = "__Atomic_Overhaul__/graphics/icons/graphite-fuel-cell.png",
-		icon_size = 64
+		icon_size = 64,
 		subgroup = "intermediate-product",
 		stack_size = 50,
 		fuel_value = "150GJ",
