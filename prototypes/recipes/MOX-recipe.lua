eeeeed5e70fb4a74e58f6761f286a8c972d894d6
Xman1109 <shawnoliver1109@gmail.com>
2021-08-03 19:39:31 +0200

    kill me

--- prototypes/recipes/MOX-recipe.lua
+++ prototypes/recipes/MOX-recipe.lua
@@ -12,7 +12,7 @@ data:extend(
         subgroup = "resources",
         ingredients =
         {
-            {"uranium-283", 9},
+            {"uranium-238", 9},
             {"plutonium", 9}
         },
         always_show_made_in = true,
