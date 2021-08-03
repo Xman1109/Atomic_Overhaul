1c8276820d9941f9602823b31fb8a9f017315c84
Xman1109 <shawnoliver1109@gmail.com>
2021-08-03 17:03:43 +0200

    graphite-fuel-cell

---
+++ prototypes/items/graphite-fuel-cell.lua
@@ -0,0 +1,14 @@
+data:extend({
+    {
+		type = "item",
+		name = "graphite-fuel-cell",
+		icon = "__Atomic_Overhaul__/graphics/icons/graphite-fuel-cell.png",
+		icon_size = 64
+		subgroup = "intermediate-product",
+		stack_size = 50,
+		fuel_value = "150GJ",
+		fuel_category = "nuclear",
+		burnt_result = "graphite-depleted-cell"
+		
+	}
+})
