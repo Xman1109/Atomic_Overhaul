cbc0f6a5deec44a4a457a5bfc8f757ecd9e8e75b
fainda <bschoen26219@gmail.com>
2021-08-03 23:55:01 +0200

    added empty fuel cell

---
+++ prototypes/items/empty-fuel-cell.lua
@@ -0,0 +1,12 @@
+data:extend({
+    {
+		type = "item",
+		name = "empty-fuel-cell",
+		icon = "__Atomic_Overhaul__/graphics/icons/empty-fuel-cell.png",
+		icon_size = 64,
+		subgroup = "intermediate-product",
+		stack_size = 50,
+		order = "aa",
+		
+	}
+})
