3b946b26978e9919766fb884af412f61fc3832db
Xman1109 <shawnoliver1109@gmail.com>
2021-08-03 03:40:09 +0200

    item-groups+vanilla changes folder

---
+++ prototypes/group.lua
@@ -0,0 +1,21 @@
+data:extend(
+    {
+        {
+            type = "item-group",
+            name = "atomic-overhaul",
+            icon = "__Atomic_Overhaul__/graphics/icons/group.png",
+            icon_size = 64,
+        },
+        {
+            type = "item-subgroup",
+            name = "intermediate-product",
+            group = "atomic-overhaul",
+            order = "a"
+        },
+        {
+            type = "item-subgroup",
+            name = "fuel-cells",
+            group = "atomic-overhaul",
+            order = "b"
+        }
+    })
