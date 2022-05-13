d656403bdb5dbc217eb19bb898350abece8658f2
Xman1109 <shawnoliver1109@gmail.com>
2022-05-13 15:24:26 +0200

    1.1.25 + alternative recipe for research data

---
+++ prototypes/recipes/research-data.lua
@@ -0,0 +1,23 @@
+data:extend(
+    {
+        {
+        type = "recipe",
+        name = "research-data-recipe",
+        icon = graphics .. "research-data.png",
+        icon_size = 64, mipmap_count = 4,
+        category = "centrifuging",
+        energy_required = 30,
+        enabled = false,
+        order = "f[utility-science-pack]-g[research-data-recipe]",
+        subgroup = "science-pack",
+        ingredients =
+        {
+            {"uranium-235", 1},
+            {"copper-plate", 2},
+            {"plastic-bar", 5}
+        },
+        results = 	{
+            {type = "item", name = "research-data", amount_min = 3, amount_max = 5},
+        },
+    }
+})
--- prototypes/vanilla-changes/technologies.lua
+++ prototypes/vanilla-changes/technologies.lua
@@ -6,6 +6,7 @@ end
 table.insert(data.raw["technology"]["nuclear-fuel-reprocessing"].prerequisites, "uranium-processing")
 table.insert(data.raw["technology"]["atomic-bomb"].prerequisites, "plutonium-processing")
 table.insert(data.raw["technology"]["atomic-bomb"].unit.ingredients, {"research-data", 1})
+table.insert(data.raw["technology"]["nuclear-fuel-reprocessing"].effects, {type = "unlock-recipe", recipe = "research-data-recipe"})
 data.raw["technology"]["uranium-processing"].effects = {{type = "unlock-recipe",recipe = "uranium-processing"},{type = "unlock-recipe",recipe = "uranium-fuel-cell"}}
 data.raw["technology"]["uranium-processing"].prerequisites = {"graphite-fuel-reprocessing"}
 data.raw["technology"]["nuclear-power"].prerequisites = {"graphite-processing"}
