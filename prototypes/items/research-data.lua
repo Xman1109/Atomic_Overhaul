c57e13ca86c5f2ba81cc71ec4bd5c3f4b516d869
Xman1109 <shawnoliver1109@gmail.com>
2022-05-21 13:06:38 +0200

    1.1.27 + Added some technology descriptions. (Needs to be translated!) + Fixed a bug where the durability of the research data was not displayed correctly when using it for research.

--- prototypes/items/research-data.lua
+++ prototypes/items/research-data.lua
@@ -3,11 +3,14 @@ data:extend(
         {
             type = "tool",
             name = "research-data",
+            localised_description = {"item-description.science-pack"},
             icon = graphics .. "research-data.png",
             icon_size = 64, icon_mipmaps = 4,
             subgroup = "resources",
             order = "z",
             durability = 1,
-            stack_size = 100
+            stack_size = 100,
+            durability_description_key = "description.science-pack-remaining-amount-key",
+            durability_description_value = "description.science-pack-remaining-amount-value"
         }
     })
