73156c58fcc58c28acbfc8d82b90e40893714c62
Xman1109 <shawnoliver1109@gmail.com>
2022-07-08 14:27:34 +0200

    1.2.0 + bugfix

--- prototypes/compatibility/space-exploration.lua
+++ prototypes/compatibility/space-exploration.lua
@@ -1,2 +1,6 @@
+if settings.startup["se-addon"].value == true then
 table.insert(data.raw["technology"]["se-antimatter-reactor"].prerequisites, "space-reactor")
 data.raw["reactor"]["nuclear-reactor"].collision_mask = data.raw["transport-belt"]["transport-belt"].collision_mask or data.raw["assembling-machine"]["assembling-machine-1"].collision_mask
+data.raw["tool"]["research-data"].order = "zz"
+data.raw["recipe"]["research-data-recipe"].order = "zz"
+end
