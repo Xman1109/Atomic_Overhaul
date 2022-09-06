40c0f26e0dc4d7cfb7364d6b53a07b6f8d30b292
Xman1109 <shawnoliver1109@gmail.com>
2022-09-06 19:18:33 +0200

    preparing for merge

--- control.lua
+++ control.lua
@@ -1,36 +1,99 @@
--- checking if the mod is loaded on a used savegame for the first time
-function checkSave()
-    game.print(
-        "\nINFO: If Atomic Overhaul has been added to a used save, you may need to troubleshoot your nuclear processing.\n")
-end
+script.on_init(function()
+    if remote.interfaces["kr-radioactivity"] then
+        remote.call("kr-radioactivity", "add_item", "plutonium-fuel-cell")
+        remote.call("kr-radioactivity", "add_item", "plutonium")
+        remote.call("kr-radioactivity", "add_item", "plutonium-fuel")
+        remote.call("kr-radioactivity", "add_item", "plutonium-depleted-cell")
+        remote.call("kr-radioactivity", "add_item", "MOX-fuel-cell")
+        remote.call("kr-radioactivity", "add_item", "MOX")
+        remote.call("kr-radioactivity", "add_item", "MOX-depleted-cell")
+        remote.call("kr-radioactivity", "add_item", "thorium-fuel-cell")
+        remote.call("kr-radioactivity", "add_item", "thorium")
+        remote.call("kr-radioactivity", "add_item", "thorium-depleted-cell")
+        if settings.startup["se-addon"].value == true and game.active_mods["space-exploration"] then
+            remote.call("kr-radioactivity", "add_item", "iridium-fuel-cell")
+            remote.call("kr-radioactivity", "add_item", "iridium-192")
+            remote.call("kr-radioactivity", "add_item", "iridium-depleted-cell")
+            remote.call("kr-radioactivity", "add_item", "holmium-fuel-cell")
+            remote.call("kr-radioactivity", "add_item", "holmium-166")
+            remote.call("kr-radioactivity", "add_item", "holmium-depleted-cell")
+            remote.call("kr-radioactivity", "add_item", "beryllium-fuel-cell")
+            remote.call("kr-radioactivity", "add_item", "beryllium-7")
+            remote.call("kr-radioactivity", "add_item", "beryllium-depleted-cell")
+        end
+    end
+end)
+script.on_configuration_changed(function()
+    if remote.interfaces["kr-radioactivity"] then
+        remote.call("kr-radioactivity", "add_item", "plutonium-fuel-cell")
+        remote.call("kr-radioactivity", "add_item", "plutonium")
+        remote.call("kr-radioactivity", "add_item", "plutonium-fuel")
+        remote.call("kr-radioactivity", "add_item", "plutonium-depleted-cell")
+        remote.call("kr-radioactivity", "add_item", "MOX-fuel-cell")
+        remote.call("kr-radioactivity", "add_item", "MOX")
+        remote.call("kr-radioactivity", "add_item", "MOX-depleted-cell")
+        remote.call("kr-radioactivity", "add_item", "thorium-fuel-cell")
+        remote.call("kr-radioactivity", "add_item", "thorium")
+        remote.call("kr-radioactivity", "add_item", "thorium-depleted-cell")
+        if settings.startup["se-addon"].value == true and game.active_mods["space-exploration"] then
+            remote.call("kr-radioactivity", "add_item", "iridium-fuel-cell")
+            remote.call("kr-radioactivity", "add_item", "iridium-192")
+            remote.call("kr-radioactivity", "add_item", "iridium-depleted-cell")
+            remote.call("kr-radioactivity", "add_item", "holmium-fuel-cell")
+            remote.call("kr-radioactivity", "add_item", "holmium-166")
+            remote.call("kr-radioactivity", "add_item", "holmium-depleted-cell")
+            remote.call("kr-radioactivity", "add_item", "beryllium-fuel-cell")
+            remote.call("kr-radioactivity", "add_item", "beryllium-7")
+            remote.call("kr-radioactivity", "add_item", "beryllium-depleted-cell")
+        end
+    end
+end)
 
-script.on_init(checkSave)
+-- checking if the mod is loaded on a used savegame for the first time
+script.on_init(function()
+    game.print(
+        "\nNOTE: If Atomic Overhaul has been added to an already existing savegame, you may need to troubleshoot your nuclear processing.\n")
+end)
 
 -- this script replaces the nuclear fuel reprocessing recipe with a custom one
 if k2_se then
-    function replace_nfr()
-        for index, force in pairs(game.forces) do
-            local technologies = force.technologies
-            local recipe = force.recipes
-            if technologies["nuclear-fuel-reprocessing"].researched then
-                recipe["ao-nuclear-fuel-reprocessing"].enabled = true
+    script.on_init(
+        function()
+            for index, force in pairs(game.forces) do
+                local technologies = force.technologies
+                local recipe = force.recipes
+                if technologies["nuclear-fuel-reprocessing"].researched then
+                    recipe["ao-nuclear-fuel-reprocessing"].enabled = true
+                end
             end
-        end
-    end
+        end)
 end
-script.on_init(replace_nfr)
 
 if bobplates then
-    function unlock_tech()
-        for index, force in pairs(game.forces) do
-            local technologies = force.technologies
-            local recipe = force.recipes
-            if recipe["empty-nuclear-fuel-reprocessing"].enabled then
-                technologies["graphite-processing"].enabled = true
+    script.on_init(
+        function()
+            for index, force in pairs(game.forces) do
+                local technologies = force.technologies
+                local recipe = force.recipes
+                if recipe["empty-nuclear-fuel-reprocessing"].enabled then
+                    technologies["ao-graphite-processing"].enabled = true
+                end
             end
-        end
-    end
+        end)
 end
+
+if bzcarbon then
+    script.on_init(
+        function()
+            for index, force in pairs(game.forces) do
+                local technologies = force.technologies
+                if technologies["ao-graphite-processing"].researched then
+                    technologies["graphite-processing"].researched = true
+                end
+            end
+        end)
+end
+
 
 -- example code from informatron
 
