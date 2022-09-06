3e8394ebf037ab0c72866eb46b3130471a8d862b
Xman1109 <shawnoliver1109@gmail.com>
2022-09-06 19:13:50 +0200

    preparing for merge with informatron branch

--- control.lua
+++ control.lua
@@ -93,3 +93,151 @@ if bzcarbon then
             end
         end)
 end
+
+
+-- example code from informatron
+
+-- Remote interface. replace "example" with your mod name
+remote.add_interface("Atomic_Overhaul", {
+    informatron_menu = function(data)
+        return example_menu(data.player_index)
+    end,
+    informatron_page_content = function(data)
+        return example_page_content(data.page_name, data.player_index, data.element)
+    end
+})
+
+function example_menu(player_index)
+    return {
+        AO_materials = 1,
+        SE_materials = 1
+    }
+end
+
+function example_page_content(page_name, player_index, element)
+    -- main page
+    if page_name == "Atomic_Overhaul" then
+        element.add {
+            type = "button",
+            name = "ao_header",
+            style = "header"
+        }
+        element.add {
+            type = "label",
+            name = "text_1",
+            caption = { "Atomic_Overhaul.page_Atomic_Overhaul_text_1" }
+        }
+    end
+
+    if page_name == "AO_materials" then
+        element.add {
+            type = "label",
+            name = "text_1",
+            caption = { "Atomic_Overhaul.page_AO_materials_text_1" }
+        }
+        element.add {
+            type = "button",
+            name = "graphite",
+            style = "graphite",
+        }
+        element.add {
+            type = "label",
+            name = "text_2",
+            caption = { "Atomic_Overhaul.page_AO_materials_text_2" }
+        }
+        element.add {
+            type = "button",
+            name = "uranium",
+            style = "uranium",
+        }
+        element.add {
+            type = "label",
+            name = "text_3",
+            caption = { "Atomic_Overhaul.page_AO_materials_text_3" }
+        }
+        element.add {
+            type = "button",
+            name = "plutonium",
+            style = "plutonium",
+        }
+        element.add {
+            type = "label",
+            name = "text_4",
+            caption = { "Atomic_Overhaul.page_AO_materials_text_4" }
+        }
+        element.add {
+            type = "button",
+            name = "MOX",
+            style = "MOX",
+        }
+        element.add {
+            type = "label",
+            name = "text_5",
+            caption = { "Atomic_Overhaul.page_AO_materials_text_5" }
+        }
+        element.add {
+            type = "button",
+            name = "thorium",
+            style = "thorium",
+        }
+        element.add {
+            type = "label",
+            name = "text_6",
+            caption = { "Atomic_Overhaul.page_AO_materials_text_6" }
+        }
+    end
+
+    if page_name == "SE_materials" then
+        element.add {
+            type = "label",
+            name = "text_1",
+            caption = { "Atomic_Overhaul.page_SE_materials_text_1" }
+        }
+        element.add {
+            type = "button",
+            name = "Space_Reactor",
+            style = "Space_Reactor",
+        }
+        element.add {
+            type = "label",
+            name = "text_2",
+            caption = { "Atomic_Overhaul.page_SE_materials_text_2" }
+        }
+        element.add {
+            type = "button",
+            name = "Iridium",
+            style = "iridium",
+        }
+        element.add {
+            type = "label",
+            name = "text_3",
+            caption = { "Atomic_Overhaul.page_SE_materials_text_3" }
+        }
+        element.add {
+            type = "button",
+            name = "Holmium",
+            style = "holmium",
+        }
+        element.add {
+            type = "label",
+            name = "text_4",
+            caption = { "Atomic_Overhaul.page_SE_materials_text_4" }
+        }
+        element.add {
+            type = "button",
+            name = "Beryllium",
+            style = "beryllium",
+        }
+        element.add {
+            type = "label",
+            name = "text_5",
+            caption = { "Atomic_Overhaul.page_SE_materials_text_5" }
+        }
+        element.add {
+            type = "button",
+            name = "glow_1",
+            style = "glow_1",
+        }
+    end
+
+end
--- data.lua
+++ data.lua
@@ -66,3 +66,20 @@ if settings.startup["thorium-wrd"].value == true then
     require(recipes .. "thorium-without-research-data")
     require(techs .. "thorium-without-research-data")
 end
+
+if mods["informatron"] then
+    -- example code from informatron
+
+    -- style_name, filename, width, height -- style_name MUST be a completely unique name
+    informatron_make_image("header", images .. "logo.png", 800, 250)
+    informatron_make_image("graphite", images .. "graphite.png", 64, 64)
+    informatron_make_image("uranium", images .. "uranium.png", 64, 64)
+    informatron_make_image("plutonium", images .. "plutonium.png", 64, 64)
+    informatron_make_image("MOX", images .. "MOX.png", 64, 64)
+    informatron_make_image("thorium", images .. "thorium.png", 64, 64)
+    informatron_make_image("Space_Reactor", images .. "Space_Reactor.png", 302, 318)
+    informatron_make_image("iridium", images .. "iridium.png", 64, 64)
+    informatron_make_image("holmium", images .. "holmium.png", 64, 64)
+    informatron_make_image("beryllium", images .. "beryllium.png", 64, 64)
+    informatron_make_image("glow_1", images .. "glow.png", 243, 100)
+end
