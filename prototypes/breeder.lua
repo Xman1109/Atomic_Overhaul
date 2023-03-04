636d07c75dfed14b6a7a4fd936d3d212d3bf519d
Alexander <95149904+garrotte13@users.noreply.github.com>
2023-03-04 13:09:50 +0300

    breeder first commit (kovarex still available)

--- prototypes/breeder.lua
+++ prototypes/breeder.lua
@@ -1,4 +1,7 @@
 breed = {}
+local breeder_graphics = "__Atomic_Overhaul__/graphics/breeder/"
+local se_addon_graphics = "__Atomic_Overhaul__/graphics/se-addon/icons/"
+require("prototypes/breeder_apm")
 
 function breed.NewEnrichment()
   data:extend({
@@ -83,18 +86,19 @@ function breed.NewEnrichment()
       -- TO_DO : allow all modules
     },
   })
-  data.raw["recipe"]["uranium-processing"].hidden = true   -- I hid it instead of using for LEU, because icon, description would be incorrect anyway.
+  data.raw["recipe"]["uranium-processing"].hidden = true -- I hid it instead of using for LEU, because icon, description would be incorrect anyway.
   data.raw["recipe"]["uranium-fuel-cell"].ingredients = { { "uranium-low-enriched", 4 }, { "iron-plate", 1 } }
   data.raw["recipe"]["uranium-fuel-cell"].result_count = 2
   data.raw["item"]["uranium-235"].localised_name = { "item-name.uranium-235-simple" }
   data.raw["item"]["uranium-238"].localised_name = { "item-name.uranium-238-simple" }
 
-    local c_recipe = data.raw.recipe["kovarex-enrichment-process"] -- fixing Kovarex for no-Breeders to utilize DU into LEU only.
+  local c_recipe = data.raw.recipe
+      ["kovarex-enrichment-process"] -- fixing Kovarex for no-Breeders to utilize DU into LEU only.
   if c_recipe then
     c_recipe.energy_required = 120
     c_recipe.allow_decomposition = false
     c_recipe.subgroup = "resources"
-    c_recipe.localised_description = {"technology-description.kovarex-simple"}
+    c_recipe.localised_description = { "technology-description.kovarex-simple" }
     for i, component in pairs(c_recipe.ingredients) do
       for _, value in pairs(component) do
         if value == "uranium-235" then
@@ -118,7 +122,7 @@ function breed.NewEnrichment()
   c_recipe = data.raw.recipe["nuclear-fuel"]
   for i, component in pairs(c_recipe.ingredients) do
     for _, value in pairs(component) do
-      if value == "uranium-235" then    -- More universal way is to check for amount of uranium-235 and put double amount of LEU.
+      if value == "uranium-235" then -- More universal way is to check for amount of uranium-235 and put double amount of LEU.
         c_recipe.ingredients[i] = { type = "item", name = "uranium-low-enriched", amount = 2 }
         break
       end
@@ -128,7 +132,7 @@ function breed.NewEnrichment()
 
   --  **** TECHNOLOGIES ****
   local process_u = data.raw["technology"]["uranium-processing"]
-  if process_u then   -- TO_DO Add unlock in control.lua for situation with unloading Schall's mod or adding AO to existing game in Simplified mode.
+  if process_u then -- TO_DO Add unlock in control.lua for situation with unloading Schall's mod or adding AO to existing game in Simplified mode.
     for i, v in pairs(process_u.effects) do
       if v.recipe == "uranium-processing" then
         table.remove(process_u.effects, i)
@@ -144,10 +148,185 @@ function breed.NewEnrichment()
   end
 end
 
+-- ********* BREEDER Reactions /////////////////
+
 function breed.Breeder()
+  if data.raw["fuel-category"]["nuclear-breeder"] then
+    if ao_debug then
+      log("Nuclear Breeder fuel category is already present.")
+    end
+  else
     data:extend({
-        
-    })
-    -- kovarex-enrichment-process
+      {
+        type = 'fuel-category',
+        name = 'nuclear-breeder'
+      } })
+  end
+  breed.APM_breeder_reactor()
+  data:extend({
+
+    -- **** ITEMS
+    {
+      type = "item",
+      name = "ao-breeder-fuel-cell",
+      icon = se_addon_graphics .. "holmium-fuel-cell.png", -- taking holmium fuel cell graphics and glow color
+      icon_size = 64,
+      icon_mipmaps = 4,
+      pictures =
+      {
+        layers =
+        {
+          {
+            size = 64,
+            filename = se_addon_graphics .. "holmium-fuel-cell.png",
+            scale = 0.25,
+            icon_mipmaps = 4
+          },
+          {
+            draw_as_light = true,
+            flags = { "light" },
+            size = 64,
+            filename = base_graphics .. "uranium-fuel-cell-light.png",
+            scale = 0.25,
+            icon_mipmaps = 4
+          }
+        }
+      },
+      fuel_category = "nuclear-breeder",
+      burnt_result = "ao-breeder-depleted-cell",
+      fuel_value = "1GJ",
+      fuel_glow_color = { 203, 79, 71 },
+      stack_size = 50,
+      group = "atomic_overhaul",
+      subgroup = "fuel-cells",
+      order = "f"
+    },
+    {
+      type = "item",
+      name = "ao-breeder-depleted-cell",
+      icon = se_addon_graphics .. "holmium-depleted-cell.png", -- taking holmium fuel cell graphics
+      icon_size = 64,
+      icon_mipmaps = 4,
+      stack_size = 50,
+      group = "atomic-overhaul",
+      order = "f",
+      subgroup = "used-up-fuel-cells",
+    },
+    {
+      type = "item",
+      name = "apm_nuclear_breeder",
+      icon = breeder_graphics .. "apm_nuclear_breeder.png",
+      icon_size = 64,
+      icon_mipmaps = 4,
+      subgroup = "energy",
+      order = "f[nuclear-energy]-a[reactor_b]",
+      place_result = "apm_nuclear_breeder",
+      stack_size = 10
+    },
+
+    -- **** RECIPES
+    {
+      type = "recipe",
+      name = "apm_nuclear_breeder-recipe",
+      icon = breeder_graphics .. "apm_nuclear_breeder.png",
+      icon_size = 64,
+      icon_mipmaps = 4,
+      ingredients = {
+        { "concrete",         500 },
+        { "steel-plate",      500 },
+        { "advanced-circuit", 500 },
+        { "copper-plate",     500 }
+      },
+      result = "apm_nuclear_breeder",
+      energy_required = 64,
+      --category = "advanced-crafting",
+      enabled = false
+    },
+    {
+      type = "recipe",
+      name = "ao-breeder-fuel-cell-recipe",
+      icon = se_addon_graphics .. "holmium-fuel-cell.png",
+      icon_size = 64,
+      icon_mipmaps = 4,
+      category = "centrifuging",
+      crafting_machine_tint = cmt.holmium,
+      energy_required = 120,
+      enabled = false,
+      order = "f",
+      subgroup = "fuel-cells",
+      ingredients =
+      {
+        { "uranium-235",          1 },
+        { "uranium-low-enriched", 7 },
+        { "uranium-238",          5 }
+      },
+      results = {
+        { "ao-breeder-fuel-cell", 4 },
+      }
+    },
+    {
+      type = "recipe",
+      name = "ao-breeder-reprocessing",
+      icon = se_addon_graphics .. "holmium-166-reprocessing-recipe.png", -- taking holmium fuel cell graphics
+      icon_size = 64,
+      icon_mipmaps = 4,
+      category = "centrifuging",
+      crafting_machine_tint = cmt.holmium,
+      energy_required = 180,
+      allow_decomposition = false,
+      enabled = false,
+      order = "f",
+      subgroup = "reprocessing",
+      ingredients =
+      {
+        { "ao-breeder-depleted-cell", 4 }
+      },
+      results = {
+        { "plutonium",        4 },
+        mods["SchallUraniumProcessing"] and { "uranium-concentrate", 39 }
+        or { "uranium-ore", 390 },
+        { "fissile-products", 4 }
+      },
+    },
+
+    -- **** TECHNOLOGIES
+    {
+      type = "technology",
+      name = "ao-breeder-reactions",
+      icon = breeder_graphics .. "apm_nuclear_breeder_tech.png",
+      icon_size = 128,
+      --icon_mipmaps = 4,
+      prerequisites = { "MOX-processing" },
+      effects =
+      {
+        {
+          type = "unlock-recipe",
+          recipe = "ao-breeder-fuel-cell-recipe"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "ao-breeder-reprocessing"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "apm_nuclear_breeder-recipe"
+        }
+      },
+      unit =
+      {
+        count = 1500,
+        ingredients =
+        {
+          { "automation-science-pack", 1 },
+          { "logistic-science-pack",   1 },
+          { "chemical-science-pack",   1 },
+          { "production-science-pack", 1 },
+        },
+        time = 45
+      }
+    }
+  })
+  -- kovarex-enrichment-process
 end
+
 return breed
---
+++ prototypes/breeder_apm.lua
@@ -0,0 +1,159 @@
+local breeder_graphics = "__Atomic_Overhaul__/graphics/breeder/"
+local breeder_sound = "__Atomic_Overhaul__/sound/breeder_working.ogg"
+require('util')
+
+-- Code of this particular mod entity Copyright (C) 2019 Bjoern Trimborn (alias Amator Phasma)
+-- GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+function breed.APM_breeder_reactor()
+    local breeder_reactor = table.deepcopy(data.raw.reactor['nuclear-reactor'])
+    breeder_reactor.type = 'reactor'
+    breeder_reactor.name = 'apm_nuclear_breeder'
+    breeder_reactor.icons = {
+        { icon = breeder_graphics .. "apm_nuclear_breeder.png", icon_size = 64 }
+    }
+
+    breeder_reactor.flags = { "placeable-neutral", "placeable-player", "player-creation" }
+    breeder_reactor.minable = { mining_time = 2, result = "apm_nuclear_breeder" }
+
+    breeder_reactor.working_sound = {}
+    breeder_reactor.working_sound.sound = {}
+    breeder_reactor.working_sound.sound.filename = breeder_sound
+    breeder_reactor.working_sound.sound.volume = 0.6
+    breeder_reactor.working_sound.apparent_volume = 1.5
+
+    breeder_reactor.consumption = "5MW"
+    breeder_reactor.neighbour_bonus = 0.2
+    breeder_reactor.energy_source = {}
+    breeder_reactor.energy_source.type = "burner"
+    breeder_reactor.energy_source.fuel_category = "nuclear-breeder"
+    breeder_reactor.energy_source.effectivity = 1
+    breeder_reactor.energy_source.fuel_inventory_size = 1
+    breeder_reactor.energy_source.burnt_inventory_size = 1
+
+    breeder_reactor.collision_box = { { -2.2, -2.2 }, { 2.2, 2.2 } }
+    breeder_reactor.selection_box = { { -2.5, -2.5 }, { 2.5, 2.5 } }
+
+    breeder_reactor.lower_layer_picture = {}
+    breeder_reactor.lower_layer_picture.filename = breeder_graphics .. "reactor/breeder_reactor_pipes.png"
+    breeder_reactor.lower_layer_picture.width = 156
+    breeder_reactor.lower_layer_picture.height = 156
+    breeder_reactor.lower_layer_picture.shift = util.by_pixel(-2, -4)
+    breeder_reactor.lower_layer_picture.hr_version = {}
+    breeder_reactor.lower_layer_picture.hr_version.filename =
+        breeder_graphics .. "reactor/hr_breeder_reactor_pipes.png"
+    breeder_reactor.lower_layer_picture.hr_version.width = 320
+    breeder_reactor.lower_layer_picture.hr_version.height = 316
+    breeder_reactor.lower_layer_picture.hr_version.scale = 0.5
+    breeder_reactor.lower_layer_picture.hr_version.shift = util.by_pixel(-1, -5)
+
+    breeder_reactor.heat_lower_layer_picture = {}
+    breeder_reactor.heat_lower_layer_picture.filename =
+        breeder_graphics .. "reactor/breeder_reactor_pipes_hot.png"
+    breeder_reactor.heat_lower_layer_picture.width = 156
+    breeder_reactor.heat_lower_layer_picture.height = 156
+    breeder_reactor.heat_lower_layer_picture.shift = util.by_pixel(-3, -4)
+    breeder_reactor.heat_lower_layer_picture.hr_version = {}
+    breeder_reactor.heat_lower_layer_picture.hr_version.filename =
+        breeder_graphics .. "reactor/hr_breeder_reactor_pipes_hot.png"
+    breeder_reactor.heat_lower_layer_picture.hr_version.width = 320
+    breeder_reactor.heat_lower_layer_picture.hr_version.height = 316
+    breeder_reactor.heat_lower_layer_picture.hr_version.scale = 0.5
+    breeder_reactor.heat_lower_layer_picture.hr_version.shift = util.by_pixel(-0.5, -4.5)
+
+    breeder_reactor.picture = {}
+    breeder_reactor.picture.layers = {}
+    breeder_reactor.picture.layers[1] = {}
+    breeder_reactor.picture.layers[1].filename =
+        breeder_graphics .. "reactor/breeder_reactor.png"
+    breeder_reactor.picture.layers[1].width = 240
+    breeder_reactor.picture.layers[1].height = 170
+    breeder_reactor.picture.layers[1].shift = { 1.375, 0 }
+    breeder_reactor.picture.layers[1].hr_version = {}
+    breeder_reactor.picture.layers[1].hr_version.filename =
+        breeder_graphics .. "reactor/hr_breeder_reactor.png"
+    breeder_reactor.picture.layers[1].hr_version.width = 480
+    breeder_reactor.picture.layers[1].hr_version.height = 340
+    breeder_reactor.picture.layers[1].hr_version.scale = 0.5
+    breeder_reactor.picture.layers[1].hr_version.shift = { 1.375, 0 }
+    breeder_reactor.picture.layers[2] = {}
+    breeder_reactor.picture.layers[2].filename =
+        breeder_graphics .. "reactor/breeder_reactor_shadow.png"
+    breeder_reactor.picture.layers[2].width = 240
+    breeder_reactor.picture.layers[2].height = 170
+    breeder_reactor.picture.layers[2].shift = { 1.375, 0 }
+    breeder_reactor.picture.layers[2].draw_as_shadow = true
+    breeder_reactor.picture.layers[2].hr_version = {}
+    breeder_reactor.picture.layers[2].hr_version.filename =
+        breeder_graphics .. "reactor/hr_breeder_reactor_shadow.png"
+    breeder_reactor.picture.layers[2].hr_version.width = 480
+    breeder_reactor.picture.layers[2].hr_version.height = 340
+    breeder_reactor.picture.layers[2].hr_version.scale = 0.5
+    breeder_reactor.picture.layers[2].hr_version.shift = { 1.375, 0 }
+    breeder_reactor.picture.layers[2].hr_version.draw_as_shadow = true
+
+    breeder_reactor.working_light_picture = {}
+    breeder_reactor.working_light_picture.filename =
+        breeder_graphics .. "reactor/breeder_reactor_glow.png"
+    breeder_reactor.working_light_picture.width = 240
+    breeder_reactor.working_light_picture.height = 170
+    breeder_reactor.working_light_picture.shift = { 1.375, 0 }
+    breeder_reactor.working_light_picture.blend_mode = "additive"
+    breeder_reactor.working_light_picture.hr_version = {}
+    breeder_reactor.working_light_picture.hr_version.filename =
+        breeder_graphics .. "reactor/hr_breeder_reactor_glow.png"
+    breeder_reactor.working_light_picture.hr_version.width = 480
+    breeder_reactor.working_light_picture.hr_version.height = 340
+    breeder_reactor.working_light_picture.hr_version.scale = 0.5
+    breeder_reactor.working_light_picture.hr_version.shift = { 1.375, 0 }
+    breeder_reactor.working_light_picture.hr_version.blend_mode = "additive"
+
+    --breeder_reactor.light = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.5, b = 0.0}}
+    breeder_reactor.use_fuel_glow_color = true                                               -- should use glow color from fuel item prototype as light color and tint for working_light_picture
+    breeder_reactor.default_fuel_glow_color = { r = 1.000, g = 0.500, b = 0.000, a = 0.500 } --color used as working_light_picture tint for fuels that don't have glow color defined
+
+    breeder_reactor.heat_buffer = {}
+    breeder_reactor.heat_buffer.max_temperature = 900
+    breeder_reactor.heat_buffer.specific_heat = "2MJ"
+    breeder_reactor.heat_buffer.max_transfer = "2GW"
+    breeder_reactor.heat_buffer.minimum_glow_temperature = 350
+    breeder_reactor.heat_buffer.glow_alpha_modifier = 0.6
+    breeder_reactor.heat_buffer.connections = {}
+    breeder_reactor.heat_buffer.connections[1] = {}
+    breeder_reactor.heat_buffer.connections[1].position = { -2, -2 }
+    breeder_reactor.heat_buffer.connections[1].direction = defines.direction.north
+    breeder_reactor.heat_buffer.connections[2] = {}
+    breeder_reactor.heat_buffer.connections[2].position = { 0, -2 }
+    breeder_reactor.heat_buffer.connections[2].direction = defines.direction.north
+    breeder_reactor.heat_buffer.connections[3] = {}
+    breeder_reactor.heat_buffer.connections[3].position = { 2, -2 }
+    breeder_reactor.heat_buffer.connections[3].direction = defines.direction.north
+    breeder_reactor.heat_buffer.connections[4] = {}
+    breeder_reactor.heat_buffer.connections[4].position = { 2, -2 }
+    breeder_reactor.heat_buffer.connections[4].direction = defines.direction.east
+    breeder_reactor.heat_buffer.connections[5] = {}
+    breeder_reactor.heat_buffer.connections[5].position = { 2, 0 }
+    breeder_reactor.heat_buffer.connections[5].direction = defines.direction.east
+    breeder_reactor.heat_buffer.connections[6] = {}
+    breeder_reactor.heat_buffer.connections[6].position = { 2, 2 }
+    breeder_reactor.heat_buffer.connections[6].direction = defines.direction.east
+    breeder_reactor.heat_buffer.connections[7] = {}
+    breeder_reactor.heat_buffer.connections[7].position = { 2, 2 }
+    breeder_reactor.heat_buffer.connections[7].direction = defines.direction.south
+    breeder_reactor.heat_buffer.connections[8] = {}
+    breeder_reactor.heat_buffer.connections[8].position = { 0, 2 }
+    breeder_reactor.heat_buffer.connections[8].direction = defines.direction.south
+    breeder_reactor.heat_buffer.connections[9] = {}
+    breeder_reactor.heat_buffer.connections[9].position = { -2, 2 }
+    breeder_reactor.heat_buffer.connections[9].direction = defines.direction.south
+    breeder_reactor.heat_buffer.connections[10] = {}
+    breeder_reactor.heat_buffer.connections[10].position = { -2, 2 }
+    breeder_reactor.heat_buffer.connections[10].direction = defines.direction.west
+    breeder_reactor.heat_buffer.connections[11] = {}
+    breeder_reactor.heat_buffer.connections[11].position = { -2, 0 }
+    breeder_reactor.heat_buffer.connections[11].direction = defines.direction.west
+    breeder_reactor.heat_buffer.connections[12] = {}
+    breeder_reactor.heat_buffer.connections[12].position = { -2, -2 }
+    breeder_reactor.heat_buffer.connections[12].direction = defines.direction.west
+    data:extend({ breeder_reactor })
+end
