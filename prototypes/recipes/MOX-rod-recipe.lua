d1c0457e76172abbeff58ce9c184efcf6f45cb89
Alexander <95149904+garrotte13@users.noreply.github.com>
2023-06-08 10:45:29 +0300

    disable rods recipes for Simplified mode

--- prototypes/recipes/MOX-rod-recipe.lua
+++ prototypes/recipes/MOX-rod-recipe.lua
@@ -1,21 +1,23 @@
-data:extend({
-    {
-        type = "recipe",
-        name = "MOX-rod-recipe",
-        icon = graphics .. "MOX-fuel-rod.png",
-        icon_size = 128,
-        icon_mipmaps = 4,
-        subgroup = "fuel-rods",
-        order = "c",
-        category = "centrifuging",
-        enabled = false,
-        energy_required = 30,
-        ingredients = {
-            {type = "item", name = "MOX", amount = 10},
-            {type = "item", name = "graphite", amount = 1}
-        },
-        results = {
-            {"MOX-fuel-rod", 10}
+if settings.startup["ao-complexity-level"].value ~= "simple" then
+    data:extend({
+        {
+            type = "recipe",
+            name = "MOX-rod-recipe",
+            icon = graphics .. "MOX-fuel-rod.png",
+            icon_size = 128,
+            icon_mipmaps = 4,
+            subgroup = "fuel-rods",
+            order = "c",
+            category = "centrifuging",
+            enabled = false,
+            energy_required = 30,
+            ingredients = {
+                { type = "item", name = "MOX",      amount = 10 },
+                { type = "item", name = "graphite", amount = 1 }
+            },
+            results = {
+                { "MOX-fuel-rod", 10 }
+            }
         }
-    }
-})
+    })
+end
--- prototypes/recipes/thorium-rod-recipe.lua
+++ prototypes/recipes/thorium-rod-recipe.lua
@@ -1,21 +1,23 @@
-data:extend({
-    {
-        type = "recipe",
-        name = "thorium-rod-recipe",
-        icon = graphics .. "thorium-fuel-rod.png",
-        icon_size = 128,
-        icon_mipmaps = 4,
-        subgroup = "fuel-rods",
-        order = "d",
-        category = "centrifuging",
-        enabled = false,
-        energy_required = 30,
-        ingredients = {
-            {type = "item", name = "thorium", amount = 10},
-            {type = "item", name = "graphite", amount = 1}
-        },
-        results = {
-            {"thorium-fuel-rod", 10}
+if settings.startup["ao-complexity-level"].value ~= "simple" then
+    data:extend({
+        {
+            type = "recipe",
+            name = "thorium-rod-recipe",
+            icon = graphics .. "thorium-fuel-rod.png",
+            icon_size = 128,
+            icon_mipmaps = 4,
+            subgroup = "fuel-rods",
+            order = "d",
+            category = "centrifuging",
+            enabled = false,
+            energy_required = 30,
+            ingredients = {
+                { type = "item", name = "thorium",  amount = 10 },
+                { type = "item", name = "graphite", amount = 1 }
+            },
+            results = {
+                { "thorium-fuel-rod", 10 }
+            }
         }
-    }
-})
+    })
+end
