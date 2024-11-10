1e33d115b2e00d3dac0eb8afce04ad2ca32a81f6
Xman1109 <shawnoliver1109@gmail.com>
2024-11-10 14:51:06 +0100

    1.3.35 + merged #69

--- migrations/1.3.0.lua
+++ migrations/1.3.0.lua
@@ -1,34 +1,38 @@
 for index, force in pairs(game.forces) do
     local technologies = force.technologies
     local recipe = force.recipes
-
     if settings.startup["old-graphite-fuel"].value == false then
         if technologies["uranium-processing"].researched then
             if settings.startup["ao-complexity-level"].value ~= "simple" then
                 technologies["non-moderated-fuel-processing"].researched = true
                 recipe["empty-fuel-cell-recipe"].enabled = true
                 recipe["non-moderated-fuel-cell-recipe"].enabled = true
-
                 technologies["non-moderated-fuel-reprocessing"].researched = true
                 recipe["non-moderated-fuel-reprocessing"].enabled = true
-
                 technologies["graphite-cooking"].researched = true
                 recipe["graphite-furnace-recipe"].enabled = true
             end
             recipe["uranium-processing"].enabled = true
             recipe["centrifuge"].enabled = true
-
             recipe["heat-furnace-recipe"].enabled = true
         end
         if settings.startup["ao-complexity-level"].value ~= "simple" then
-            recipe["graphite-fuel-cell-recipe"].enabled = false
-            recipe["graphite-fuel-reprocessing"].enabled = false
-            recipe["graphite-recipe"].enabled = false
+            if recipe["graphite-fuel-cell-recipe"] then
+                recipe["graphite-fuel-cell-recipe"].enabled = false
+            end
+            if recipe["graphite-fuel-reprocessing"] then
+                recipe["graphite-fuel-reprocessing"].enabled = false
+            end
+            if recipe["graphite-recipe"] then
+                recipe["graphite-recipe"].enabled = false
+            end
 
-            technologies["ao-graphite-processing"].researched = false
-            technologies["graphite-fuel-reprocessing"].researched = false
-            technologies["ao-graphite-processing"].enabled = false
-            technologies["graphite-fuel-reprocessing"].enabled = false
+            if technologies["ao-graphite-processing"] and technologies["graphite-processing"] then
+                technologies["ao-graphite-processing"].researched = false
+                technologies["graphite-fuel-reprocessing"].researched = false
+                technologies["ao-graphite-processing"].enabled = false
+                technologies["graphite-fuel-reprocessing"].enabled = false
+            end
         end
     end
 end
