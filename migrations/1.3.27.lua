b5ce3f0e3b62c8101195b63ccf8fb9a195c3ba30
Alexander <95149904+garrotte13@users.noreply.github.com>
2026-01-02 13:05:37 +0300

    simple mode fix in migrations

--- migrations/1.3.27.lua
+++ migrations/1.3.27.lua
@@ -2,7 +2,7 @@ for index, force in pairs(game.forces) do
     local technologies = force.technologies
     local recipe = force.recipes
 
-        if technologies["non-moderated-fuel-processing"].researched then
+        if settings.startup["ao-complexity-level"].value ~= "simple" and technologies["non-moderated-fuel-processing"].researched then
             recipe["centrifuge"].enabled = true
         end
     end
--- migrations/1.4.lua
+++ migrations/1.4.lua
@@ -2,7 +2,7 @@ for index, force in pairs(game.forces) do
     local technologies = force.technologies
     local recipe = force.recipes
 
-        if technologies["heating-oil"].researched then
+        if settings.startup["ao-complexity-level"].value ~= "simple" and technologies["heating-oil"].researched then
             recipe["heat-tank-bottling"].enabled = true
         end
     end
