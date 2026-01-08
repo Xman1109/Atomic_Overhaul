4fa063ab188020cd306d42dfedab70127cf5dd6e
Alexander <95149904+garrotte13@users.noreply.github.com>
2026-01-08 12:07:35 +0300

    simple mode fix-2 in migrations

--- migrations/1.3.22.lua
+++ migrations/1.3.22.lua
@@ -2,7 +2,7 @@ for index, force in pairs(game.forces) do
     local technologies = force.technologies
     local recipe = force.recipes
 
-        if technologies["nuclear-fuel-reprocessing"].researched then
+        if technologies["nuclear-fuel-reprocessing"].researched and settings.startup["ao-complexity-level"].value ~= "simple" then
             recipe["research-data-recipe"].enabled = true
         end
     end
--- migrations/1.3.34.lua
+++ migrations/1.3.34.lua
@@ -2,31 +2,33 @@ for index, force in pairs(game.forces) do
     local technologies = force.technologies
     local recipe = force.recipes
 
-    if technologies["uranium-processing"].researched then
-        technologies["non-moderated-fuel-processing"].researched = true
-        technologies["non-moderated-fuel-processing"].enabled = true
-        technologies["graphite-cooking"].researched = true
-        technologies["graphite-cooking"].enabled = true
-        technologies["non-moderated-fuel-reprocessing"].enabled = true
-        technologies["non-moderated-fuel-reprocessing"].researched = true
-        technologies["nuclear-power"].researched = true
-        technologies["nuclear-power"].enabled = true
-        technologies["uranium-mining"].researched = true
-        technologies["uranium-mining"].enabled = true
-        if recipe["empty-fuel-cell-recipe"].enabled == false then
-            recipe["empty-fuel-cell-recipe"].enabled = true
+    if settings.startup["ao-complexity-level"].value ~= "simple" then
+        if technologies["uranium-processing"].researched then
+            technologies["non-moderated-fuel-processing"].researched = true
+            technologies["non-moderated-fuel-processing"].enabled = true
+            technologies["graphite-cooking"].researched = true
+            technologies["graphite-cooking"].enabled = true
+            technologies["non-moderated-fuel-reprocessing"].enabled = true
+            technologies["non-moderated-fuel-reprocessing"].researched = true
+            technologies["nuclear-power"].researched = true
+            technologies["nuclear-power"].enabled = true
+            technologies["uranium-mining"].researched = true
+            technologies["uranium-mining"].enabled = true
+            if recipe["empty-fuel-cell-recipe"].enabled == false then
+                recipe["empty-fuel-cell-recipe"].enabled = true
+            end
+            if recipe["graphite-furnace-recipe"].enabled == false then
+                recipe["graphite-furnace-recipe"].enabled = true
+            end
+            if recipe["heat-furnace-recipe"].enabled == false then
+                recipe["heat-furnace-recipe"].enabled = true
+            end
+            if recipe["non-moderated-fuel-cell-recipe"].enabled == false then
+                recipe["non-moderated-fuel-cell-recipe"].enabled = true
+            end
         end
-        if recipe["graphite-furnace-recipe"].enabled == false then
-            recipe["graphite-furnace-recipe"].enabled = true
+        if technologies["nuclear-fuel-reprocessing"].researched then
+            recipe["research-data-recipe"].enabled = true
         end
-        if recipe["heat-furnace-recipe"].enabled == false then
-            recipe["heat-furnace-recipe"].enabled = true
-        end
-        if recipe["non-moderated-fuel-cell-recipe"].enabled == false then
-            recipe["non-moderated-fuel-cell-recipe"].enabled = true
-        end
-    end
-    if technologies["nuclear-fuel-reprocessing"].researched then
-        recipe["research-data-recipe"].enabled = true
     end
 end
